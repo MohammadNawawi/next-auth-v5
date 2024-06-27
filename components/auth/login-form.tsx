92928f4cb80db4c4913a76a8f6d5c8ac8455ce25
Mohammad Nawawi <tikp2.e24m.nawawi@gmail.com>
2024-06-27 14:21:36 +0700

    feat : add form

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,6 +1,29 @@
+"use client";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginSchema } from "@/schemas";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
 import { CardWrapper } from "@/components/auth/card-wrapper";
+import { z } from "zod";
+import { Input } from "@/components/ui/input";
 
 export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   return (
     <CardWrapper
       headerLabel="Sign in"
@@ -8,7 +31,24 @@ export const LoginForm = () => {
       backButtonHref="/"
       showSocial
     >
-      Login Form
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(() => {})} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
+        </form>
+      </Form>
     </CardWrapper>
   );
 };
